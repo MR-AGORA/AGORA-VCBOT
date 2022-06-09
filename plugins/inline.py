c5e93f54e84bc3b03341558e7f67ff6ea4822502
MR AGORA <106959345+MR-AGORA@users.noreply.github.com>
2022-06-09 23:56:55 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -31,8 +31,8 @@ from pyrogram import (
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/MR-AGORA'),
+        InlineKeyboardButton('🧩 Join Here', url='https://t.me/AGORA_ROBOTS'),
     ]
     ]
 def get_cmd(dur):
